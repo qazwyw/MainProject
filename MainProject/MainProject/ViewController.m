8bf0ba3d147348aff540ac4cc2c409f1c8d5e83e
Yvan <apple@YvandeiMac.local>
2017-10-23 15:25:58 +0800

    init

---
+++ MainProject/MainProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MainProject
+//
+//  Created by Apple on 2017/10/23.
+//  Copyright © 2017年 Yvan. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "<#header#>"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
